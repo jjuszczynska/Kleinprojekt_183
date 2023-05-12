208a501c68cdcf1ef67638558eda5576aa8ff710
jj-c3p0 <60003953+jjuszczynska@users.noreply.github.com>
2023-05-12 18:19:28 +0200

    clean up

--- src/main/java/kleinProjekt_JJ/kleinprojekt/PrimaryController.java
+++ src/main/java/kleinProjekt_JJ/kleinprojekt/PrimaryController.java
@@ -5,9 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,242 +30,282 @@ import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
 
 public class PrimaryController {
-	private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$";
+    private String customRegex;
 
-	@FXML
-	private TextField tfDestination;
+    @FXML
+    private TextField tfDestination;
 
-	@FXML
-	private TextField tfPath;
+    @FXML
+    private TextField tfPath;
 
-	@FXML
-	private PasswordField pfPass;
+    @FXML
+    private PasswordField pfPass;
 
-	@FXML
-	private CheckBox cbPath;
+    @FXML
+    private CheckBox cbPath;
 
-	@FXML
-	private Button btnEncrypt;
+    @FXML
+    private Button btnEncrypt;
 
-	@FXML
-	private Button btnDecrypt;
+    @FXML
+    private Button btnDecrypt;
 
-	@FXML
-	private Pane pDrop;
+    @FXML
+    private Pane pDrop;
 
-	@FXML
-	private Label lbSum1;
+    @FXML
+    private Label lbSum1;
 
-	@FXML
-	private Label lbSum2;
+    @FXML
+    private Label lbChecksumm;
 
-	@FXML
-	private Label lbChecksumm;
+    @FXML
+    private CheckBox cbCustomRegex;
 
-	@FXML
-	private void onDragDropped(DragEvent event) {
-		Dragboard db = event.getDragboard();
-		boolean success = false;
-		if (db.hasFiles()) {
-			success = true;
-			File file = db.getFiles().get(0);
-			String path = file.getAbsolutePath();
-			tfPath.setText(path);
-		}
-		event.setDropCompleted(success);
-		event.consume();
-	}
+    @FXML
+    private TextField tfCostomRegex;
 
-	@FXML
-	private void onDragOver(DragEvent event) {
-		if (event.getGestureSource() != pDrop) {
-			if (event.getDragboard().hasFiles()) {
-				event.acceptTransferModes(TransferMode.COPY);
-				event.consume();
-			}
-		}
-	}
+    @FXML
+    private Button btnConfirmRegex;
 
-	@FXML
-	private void onDecrypt(ActionEvent event) throws Exception {
-		String filePath = tfPath.getText();
-		String password = pfPass.getText();
-		File inputFile = new File(filePath);
+    @FXML
+    private void onDragDropped(DragEvent event) {
+        Dragboard db = event.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            success = true;
+            File file = db.getFiles().get(0);
+            String path = file.getAbsolutePath();
+            tfPath.setText(path);
+        }
+        event.setDropCompleted(success);
+        event.consume();
+    }
 
-		//check if all imputs are valid
-		if (!validatePass(password)) {
-			JOptionPane.showMessageDialog(null,
-					"Invalid password. Please try again.\nPassword must have at least one uppercase letter,\none lowercase letter, one digit,\nand one special character among @#$%^&+=, be 8-20 characters long, and contain no whitespace.");
+    @FXML
+    private void onDragOver(DragEvent event) {
+        if (event.getGestureSource() != pDrop) {
+            if (event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY);
+                event.consume();
+            }
+        }
+    }
 
-			return;
-		}
+    @FXML
+    private void onDecrypt(ActionEvent event) throws Exception {
+        String filePath = tfPath.getText();
+        String password = pfPass.getText();
+        File inputFile = new File(filePath);
 
-		if (!inputFile.exists()) {
-			JOptionPane.showMessageDialog(null, "Invalid path. Please try again.");
-			return;
-		}
+        //check if all imputs are valid
+        if (password == "") {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid password. Please try again.");
 
-		if (!cbPath.isSelected() && tfDestination.getText() == "") {
-			JOptionPane.showMessageDialog(null,
-					"Destination path was not selected.\nChoose destination Path\nor check to override the orginal file");
-			return;
-		}
+            return;
+        }
 
-		// read the salt and encrypted bytes from the input file
-		byte[] salt = new byte[16];
-		byte[] encryptedBytes;
+        if (!inputFile.exists()) {
+            JOptionPane.showMessageDialog(null, "Invalid path. Please try again.");
+            return;
+        }
 
-		try (FileInputStream fis = new FileInputStream(inputFile)) {
-			fis.read(salt);
-			encryptedBytes = fis.readAllBytes();
+        if (!cbPath.isSelected() && tfDestination.getText() == "") {
+            JOptionPane.showMessageDialog(null,
+                    "Destination path was not selected.\nChoose destination Path\nor check to override the orginal file");
+            return;
+        }
 
-			SecretKey secretKey = generateKey(password, salt);
+        // read the salt and encrypted bytes from the input file
+        byte[] salt = new byte[16];
+        byte[] encryptedBytes;
 
-			// initialize the cipher in decrypt mode with ECB mode
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        try (FileInputStream fis = new FileInputStream(inputFile)) {
+            fis.read(salt);
+            encryptedBytes = fis.readAllBytes();
 
-			// decrypt the encrypted bytes
-			byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            SecretKey secretKey = generateKey(password, salt);
 
-			// write the decrypted bytes to a file
-			String outputPath = cbPath.isSelected() ? tfPath.getText() : tfDestination.getText();
-			File outputFile = new File(outputPath);
-			FileOutputStream fos = new FileOutputStream(outputFile);
-			fos.write(decryptedBytes);
-			fos.close();
+            // initialize the cipher in decrypt mode with ECB mode
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-			lbSum2.setVisible(true);
-			lbSum2.setText("After: " + checkSumm(outputPath));
+            // decrypt the encrypted bytes
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
 
-			// display success message
-			JOptionPane.showMessageDialog(null, "File decrypted successfully.");
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Decryption failed.");
-		}
+            // write the decrypted bytes to a file
+            String outputPath = cbPath.isSelected() ? tfPath.getText() : tfDestination.getText();
+            File outputFile = new File(outputPath);
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.write(decryptedBytes);
+            fos.close();
 
-	}
+            lbSum1.setVisible(true);
+            lbSum1.setText("Checksum: " + checkSumm(outputPath));
+            System.out.println(checkSumm(outputPath));
 
-	@FXML
-	private void onEncrypt(ActionEvent event) throws Exception {
-		String filePath = tfPath.getText();
-		String password = pfPass.getText();
-		File inputFile = new File(filePath);
+            // display success message
+            JOptionPane.showMessageDialog(null, "File decrypted successfully.");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Decryption failed.");
+        }
 
-		// check if all inputs are valid
-		if (!validatePass(password)) {
-			JOptionPane.showMessageDialog(null,
-					"Invalid password. Please try again.\nPassword must have at least one uppercase letter,\none lowercase letter, one digit,\nand one special character among @#$%^&+=, be 8-20 characters long, and contain no whitespace.");
-			return;
-		}
+    }
 
-		if (!inputFile.exists()) {
-			JOptionPane.showMessageDialog(null, "Invalid path. Please try again.");
-			return;
-		}
+    @FXML
+    private void onEncrypt(ActionEvent event) throws Exception {
+        String filePath = tfPath.getText();
+        String password = pfPass.getText();
+        File inputFile = new File(filePath);
 
-		if (!cbPath.isSelected() && tfDestination.getText() == "") {
-			JOptionPane.showMessageDialog(null,
-					"Destination path was not selected.\nChoose destination Path\nor check to override the orginal file");
-			return;
-		}
+        // check if all inputs are valid
+        if (!validatePass(password)) {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid password. Please try again.");
+            return;
+        }
 
-		// checksum before encryption
-		lbSum1.setVisible(true);
-		lbSum1.setText("Before: " + checkSumm(filePath));
+        if (!inputFile.exists()) {
+            JOptionPane.showMessageDialog(null, "Invalid path. Please try again.");
+            return;
+        }
 
-		try {
-			// read the input file
-			byte[] inputBytes = Files.readAllBytes(inputFile.toPath());
-			
-			//generate salt
-			byte[] salt = new byte[16];
-			SecureRandom random = new SecureRandom();
-			random.nextBytes(salt);
+        if (!cbPath.isSelected() && tfDestination.getText() == "") {
+            JOptionPane.showMessageDialog(null,
+                    "Destination path was not selected.\nChoose destination Path\nor check to override the orginal file");
+            return;
+        }
 
-			SecretKey secretKey = generateKey(password, salt);
+        // checksum before encryption
+        lbSum1.setVisible(true);
+        lbSum1.setText("Checksum: " + checkSumm(filePath));
+        System.out.println(checkSumm(filePath));
 
-			// initialize the cipher in encrypt mode with ECB mode
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        try {
+            // read the input file
+            byte[] inputBytes = Files.readAllBytes(inputFile.toPath());
 
-			// encription
-			byte[] encryptedBytes = cipher.doFinal(inputBytes);
+            //generate salt
+            byte[] salt = new byte[16];
+            SecureRandom random = new SecureRandom();
+            random.nextBytes(salt);
 
-			// write the salt and encrypted bytes to a file
-			String outputPath = cbPath.isSelected() ? tfPath.getText() : tfDestination.getText();
-			File outputFile = new File(outputPath);
-			FileOutputStream fos = new FileOutputStream(outputFile);
-			fos.write(salt);
-			fos.write(encryptedBytes);
-			fos.close();
-			
-			//checksum after encryption
-			lbSum2.setVisible(true);
-			lbSum2.setText("After: " + checkSumm(outputPath));
+            SecretKey secretKey = generateKey(password, salt);
 
-			// display success message
-			JOptionPane.showMessageDialog(null, "File encrypted successfully.");
-		} catch (Exception e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Encryption failed. Please try again.");
-		}
+            // initialize the cipher in encrypt mode with ECB mode
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
-	}
+            // encription
+            byte[] encryptedBytes = cipher.doFinal(inputBytes);
 
-	//disable destination textfield if checkbox is selected
-	@FXML
-	private void onCheckBox(ActionEvent event) {
+            // write the salt and encrypted bytes to a file
+            String outputPath = cbPath.isSelected() ? tfPath.getText() : tfDestination.getText();
+            File outputFile = new File(outputPath);
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.write(salt);
+            fos.write(encryptedBytes);
+            fos.close();
 
-		if (!cbPath.isSelected()) {
-			tfDestination.setDisable(false);
-			return;
-		}
+            // display success message
+            JOptionPane.showMessageDialog(null, "File encrypted successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Encryption failed. Please try again.");
+        }
 
-		tfDestination.clear();
-		tfDestination.setDisable(true);
+    }
 
-	}
+    //disable destination textfield if checkbox is selected
+    @FXML
+    private void onCheckBox(ActionEvent event) {
 
-	private boolean validatePass(String pass) {
+        if (!cbPath.isSelected()) {
+            tfDestination.setDisable(false);
+            return;
+        }
 
-		Pattern pattern = Pattern.compile(PASSWORD_REGEX);
-		Matcher matcher = pattern.matcher(pass);
-		return matcher.matches();
+        tfDestination.clear();
+        tfDestination.setDisable(true);
 
-	}
+    }
 
-	//compute checkSumm
-	String checkSumm(String path) throws Exception {
+    private boolean validatePass(String pass) {
+        String regexToCheck = PASSWORD_REGEX;
+        if (cbCustomRegex.isSelected()) {
+            regexToCheck = customRegex;
+        }
+        Pattern pattern = Pattern.compile(regexToCheck);
+        Matcher matcher = pattern.matcher(pass);
+        return matcher.matches();
 
-		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		FileInputStream fis = new FileInputStream(path);
+    }
 
-		byte[] dataBytes = new byte[1024];
+    //compute checkSumm
+    private String checkSumm(String path) throws Exception {
 
-		int nread = 0;
-		while ((nread = fis.read(dataBytes)) != -1) {
-			digest.update(dataBytes, 0, nread);
-		}
-		fis.close();
-		byte[] mdbytes = digest.digest();
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        FileInputStream fis = new FileInputStream(path);
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < mdbytes.length; i++) {
-			sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-		}
-		return sb.toString();
+        byte[] dataBytes = new byte[1024];
 
-	}
-	
-	//generate Key
-	private SecretKey generateKey(String key, byte[] salt) throws Exception {
+        int nread = 0;
+        while ((nread = fis.read(dataBytes)) != -1) {
+            digest.update(dataBytes, 0, nread);
+        }
+        fis.close();
+        byte[] mdbytes = digest.digest();
 
-		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-		KeySpec spec = new PBEKeySpec(key.toCharArray(), salt, 10000, 256);
-		SecretKey tmp = factory.generateSecret(spec);
-		SecretKey secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-		return secretKey;
-	}
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mdbytes.length; i++) {
+            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
 
+    }
+
+    //generate Key
+    private SecretKey generateKey(String key, byte[] salt) throws Exception {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(key.toCharArray(), salt, 10000, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKey secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+        return secretKey;
+    }
+
+    @FXML
+    private void onCbCustomRegex(ActionEvent event) {
+        if (!cbCustomRegex.isSelected()) {
+            tfCostomRegex.setDisable(true);
+            btnConfirmRegex.setDisable(true);
+
+            tfCostomRegex.clear();
+            return;
+        }
+        btnEncrypt.setDisable(true);
+        tfCostomRegex.setDisable(false);
+        btnConfirmRegex.setDisable(false);
+
+    }
+
+    @FXML
+    private void onConfirm(ActionEvent event) {
+
+        if (tfCostomRegex.getText() == "") {
+            JOptionPane.showMessageDialog(null, "Regex can't be empty. Please try it again.");
+            return;
+        }
+        if (tfCostomRegex.getText() == "^$") {
+            JOptionPane.showMessageDialog(null, "Password must be at least 1 character long. Please try it again.");
+            return;
+
+        }
+        customRegex = tfCostomRegex.getText();
+        btnEncrypt.setDisable(false);
+
+
+    }
 }
